48c1008e433f368c80d22e6f03fd24b60d9873e3
lizpku <master_lee@live.com>
2020-05-07 17:30:25 +0800

    5/7

--- app/src/main/java/com/example/pkuscheduler/Activities/SplashActivity.java
+++ app/src/main/java/com/example/pkuscheduler/Activities/SplashActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Window;
 
 import com.example.pkuscheduler.R;
 
@@ -13,7 +14,11 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_splash);
+        getSupportActionBar().hide();
+
+
         if(getSharedPreferences("loginInfo", Context.MODE_PRIVATE).getBoolean("isLogged",false)){
             startActivity(new Intent(this, TodayActivity.class));
         }
